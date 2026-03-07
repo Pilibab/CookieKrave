a94389710c98d6b081e8db1815fcf9c89a37358a
Prometheus <toshiyasato219@gmail.com>
2026-03-07 21:25:25 +0900

    fix: name -> ingredient_name name is a reserved keyword in sql

--- backend/app/db/03_inventory.sql
+++ backend/app/db/03_inventory.sql
@@ -1,6 +1,6 @@
 CREATE TABLE INVENTORY (
     inventory_id SERIAL PRIMARY KEY,
-    name VARCHAR(64),
+    ingredient_name VARCHAR(64),
     current_stock DECIMAL(10,3) DEFAULT 0.000,
     unit_of_measure VARCHAR(10) NOT NULL, -- 'g', 'ml', 'pcs', etc.
     reorder_trigger INT
