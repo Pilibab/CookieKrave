52cc1be11d83daae795b7443368ea253a4cec761
Prometheus <toshiyasato219@gmail.com>
2026-04-07 21:58:02 +0900

    feat: add shelf life to product

--- backend/app/db/02_products.sql
+++ backend/app/db/02_products.sql
@@ -3,5 +3,6 @@ CREATE TABLE PRODUCTS (
     product_name VARCHAR(64) NOT NULL,
     product_desc VARCHAR(255) NOT NULL,
     price DECIMAL (7, 2) NOT NULL, -- till xxxxx.xx
-    is_available BOOLEAN DEFAULT true
+    is_available BOOLEAN DEFAULT true,
+    shelf_life DATE NOT NULL
 );
