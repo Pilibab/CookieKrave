4885f1284edfa11c06a117c90c71739eb690ef23
Prometheus <toshiyasato219@gmail.com>
2026-04-08 22:02:27 +0900

    feat: check for PRODUCTS.price

--- backend/app/db/02_products.sql
+++ backend/app/db/02_products.sql
@@ -1,8 +1,11 @@
 CREATE TABLE PRODUCTS (
-    product_id SERIAL PRIMARY KEY,
+    product_id INT PRIMARY KEY,
     product_name VARCHAR(64) NOT NULL,
     product_desc VARCHAR(255) NOT NULL,
     price DECIMAL (7, 2) NOT NULL, -- till xxxxx.xx
     is_available BOOLEAN DEFAULT true,
     shelf_life DATE NOT NULL
+
+CONSTRAINT chk_PRODUCT_PRICE
+CHECK (price >= 0)
 );
