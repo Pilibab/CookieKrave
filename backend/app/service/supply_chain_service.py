0bbc2a280a472daf7746d81b9095c36792cb9d84
Prometheus <toshiyasato219@gmail.com>
2026-04-27 18:00:00 +0900

    feat: add update service for inventory when order is fullfilled

---
+++ backend/app/service/supply_chain_service.py
@@ -0,0 +1,43 @@
+# from typing import List
+from app.repository.bom_repo import BOMRepository
+from app.repository.inventory_repo import InventoryRepository
+from app.repository.cart_repo import CartRepository # Don't forget to import this!
+
+# from app.model.order import OrderCreate
+
+class SupplyChainService:
+    def __init__(
+        self, 
+        bom_repo: BOMRepository, 
+        inventory_repo: InventoryRepository, 
+        cart_repo: CartRepository
+    ):
+        self.bom_repo = bom_repo
+        self.inventory_repo = inventory_repo
+        self.cart_repo = cart_repo 
+
+    def update_inventory(self, order_id: int | str):
+        """
+            updates the inventory when an order is made
+        """
+        # retrieve all instance with order id from cart 
+        # purpose is to get the product id 
+        items = self.cart_repo.get_items_by_order(order_id)
+
+        for item in items:
+            #get all INV_ID that has matching PROD_ID
+            recipe = self.bom_repo.get_stock(item.PROD_ID)
+
+            for ingredient in recipe:
+                # Get the raw material info from Inventory
+                total_needed : float = ingredient.BOM_QUAN_REQ * item.CART_QUAN
+                material_id : int = ingredient.INV_ID
+
+                # Logic to subtract stock 
+                # sends the negative value of the amount req for decrement hehehe
+                self.inventory_repo.adjust_stock(material_id, - total_needed)
+
+
+
+        
+
