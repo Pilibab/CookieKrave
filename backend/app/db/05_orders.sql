48d3b1030f98c032ff6130ae3c3c1c0fc06bf464
Prometheus <toshiyasato219@gmail.com>
2026-05-02 18:12:44 +0900

    feat: refactr website

--- backend/app/db/05_orders.sql
+++ backend/app/db/05_orders.sql
@@ -1,13 +1,23 @@
 CREATE TABLE ORDERS (
     ORD_ID SERIAL PRIMARY KEY,
     CUST_ID int NOT NULL,
+    FULFILLMENT_ID int NOT NULL,
     ORD_TIME TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     -- total_amount DECIMAL (7, 2) NOT NULL,        -- derived attr hence reducted 
     ORD_PAY_METH VARCHAR(64),                       -- BOth payment and fullfillment method should be its own entity i think 
                                                     -- well see how it goes    
-    ORD_F_TYPE VARCHAR(64),                         -- fullfillment type             
+
+    ORD_STATUS VARCHAR(25) DEFAULT 'PENDING',
+    ORD_FULFILLMENT_TIME TIMESTAMP,
+
+    CONSTRAINT chk_ORD_STATUS 
+        CHECK (ORD_STATUS IN ('PENDING', 'DELIVERE D')),
 
     CONSTRAINT FK_customer_id
         FOREIGN KEY (CUST_ID) 
-        REFERENCES CUSTOMERS(CUST_ID)
+        REFERENCES CUSTOMERS(CUST_ID),
+
+    CONSTRAINT FK_fullfillment_id
+        FOREIGN KEY (FULFILLMENT_ID) 
+        REFERENCES FULFILLMENT(FULFILLMENT_ID)
 ); 
--- backend/app/db/09_invoices.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE INVOICES (
-    ORD_ID int NOT NULL,
-
-    INVOICE_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP,   -- The specific time the bill was made
-    INVOICE_DETS VARCHAR(255),                          -- should be its own class ig, INVOICE DETAIL
-    CONSTRAINT FK_order_id 
-        FOREIGN KEY (ORD_ID) 
-        REFERENCES ORDERS(ORD_ID)
-);
