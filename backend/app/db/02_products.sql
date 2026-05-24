3d39af4fc09cabe23480700f81b2c17e873c0c63
Prometheus <toshiyasato219@gmail.com>
2026-05-24 22:58:33 +0900

    feat: add image attribute and default value

--- backend/app/db/02_products.sql
+++ backend/app/db/02_products.sql
@@ -5,7 +5,7 @@ CREATE TABLE PRODUCTS (
     PROD_PRICE DECIMAL (7, 2) NOT NULL, -- till xxxxx.xx
     PROD_AVAILABLE BOOLEAN DEFAULT true,
     PROD_SL DATE NOT NULL               -- SHELF LIFE 
-
+    PROD_IMAGE_URL VARCHAR(512) DEFAULT 'https://ghhowjijwfgffcbjlsxl.supabase.co/storage/v1/object/public/product_img/default_no_img.png';
 CONSTRAINT chk_PROD_PRICE
 CHECK (PROD_PRICE >= 0)
 );
