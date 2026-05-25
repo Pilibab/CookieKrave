3a3f719b1fa3802503d1c8b4d2e082d74c864802
Prometheus <toshiyasato219@gmail.com>
2026-05-25 12:08:19 +0900

    feat: add supabase admin key, and initialize supabase client with admin key

--- backend/app/db/supabase_client.py
+++ backend/app/db/supabase_client.py
@@ -2,6 +2,8 @@ from supabase import create_client
 from app.config import configs
 
 SUPABASE_URL = configs.SUPABASE_URL.get_secret_value()
+SUPABASE_KEY_ADMIN = configs.SUPABASE_KEY_ADMIN.get_secret_value()
 SUPABASE_KEY = configs.SUPABASE_KEY.get_secret_value()
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase_admin = create_client(SUPABASE_URL, SUPABASE_KEY_ADMIN)
