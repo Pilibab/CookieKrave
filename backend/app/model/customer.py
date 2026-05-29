06134bb5abce1cc9d0eb8ca23b5bda460760e6b5
Prometheus <toshiyasato219@gmail.com>
2026-05-29 12:30:33 +0900

    fix: move pk to base model since jwt token will be used as pk

--- backend/app/model/customer.py
+++ backend/app/model/customer.py
@@ -7,18 +7,17 @@ class CustomerBase(BaseModel):
     """
         used for reading data from the data base
     """
-    cust_firstname: str = Field(min_length=5, max_length=85)
-    cust_lastname: str = Field(min_length=5, max_length=85)
+    cust_id: UUID
+    cust_firstname: str = Field(max_length=85)
+    cust_lastname: str = Field(max_length=85)
     cust_middlename: Optional[str] = Field(max_length=85)
     cust_email: EmailStr
     cust_social_provider: Optional[str] = Field(None, pattern="^(google|facebook)$")
     cust_cont_no: Optional[str] = Field(..., max_length=20) # store in +63 format
 
 
-
 class Customer(CustomerBase):
     #supabase auto creates the field value 
-    cust_id: UUID
     cust_cd: datetime
 
     # This allows Pydantic to work with SQLAlchemy/SQLModel objects
