d9e118ffdf67a4ba2c2924abbff862912c7ef5de
Prometheus <toshiyasato219@gmail.com>
2026-05-30 16:43:18 +0900

    WIP: remove comments, added path to dashboard if admin signed in

--- frontend/src/app/auth/callback-loading/page.tsx
+++ frontend/src/app/auth/callback-loading/page.tsx
@@ -9,12 +9,9 @@ export default function AuthCallbackLoading() {
     const hasRunPipeline = useRef(false);
 
     useEffect(() => {
-        console.log("Full URL:", window.location.href);
-        console.log("Hash:", window.location.hash);
-        console.log("Search:", window.location.search);
+
         // Give Supabase time to detect and exchange the hash token
         const { data: { subscription } } = supabase.auth.onAuthStateChange( async (event, session) => {
-            console.log("Auth event:", event, session); // debug
 
             if (event === "SIGNED_IN" && session) {
                 if (hasRunPipeline.current) return;
@@ -30,18 +27,26 @@ export default function AuthCallbackLoading() {
                             "Authorization": `Bearer ${token}`
                         }
                     });
-                    console.log(response);
+                    const data = await response.json()
+                    // console.log("DATA", data);
                     
-                    if (response.ok) {
+                    if (response.ok) {       
+                        // save cookie to local storage   
                         document.cookie = `sb-access-token=${token}; path=/; max-age=3600; SameSite=Lax`;
-                        console.log("response ok, proceeding");
-                        router.push("/");
+
+                        if (data.is_admin )
+                        {
+                            // if log in is admin 
+                            router.push("/dashboard");
+                        } else {
+                            // user is customer 
+                            console.log("customer is logged in ");
+                            
+                        }
                     } else {
-                        console.error("Backend rejected:", await response.text());
                         router.push("/auth/login");
                     }
                 } catch (err) {
-                    console.error("Network error:", err);
                     router.push("/auth/login");
                 }
 
@@ -49,14 +54,12 @@ export default function AuthCallbackLoading() {
                 // No session at all even after init — bail out
                 router.push("/auth/login");
             }
-            console.log("Auth event:", event, session); // debug
 
         });
 
         // Safety net — if nothing fires in 10 seconds, redirect
         const timeout = setTimeout(() => {
             if (!hasRunPipeline.current) {
-                console.error("Auth timeout — no session detected.");
                 router.push("/auth/login");
             }
         }, 10000);
