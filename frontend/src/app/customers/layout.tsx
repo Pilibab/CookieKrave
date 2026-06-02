ae8bc8164660c629fbc23f3cb92475992ff477bc
Prometheus <toshiyasato219@gmail.com>
2026-06-03 06:16:10 +0900

    refactor: simplify customer data fetching and layout structure

--- frontend/src/app/customers/layout.tsx
+++ frontend/src/app/customers/layout.tsx
@@ -1,13 +1,14 @@
-import AdminGuard from "../../components/layout/AdminGuard";
+// import AdminGuard from "../../components/layout/AdminGuard";
+
+import AdminNavbar from "@/components/layout/AdminNavbar";
 
 export default function PanelLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AdminGuard>
       <div className="min-h-screen bg-[var(--cream)]">
+        <AdminNavbar />
         <div className="pt-[calc(var(--navbar-h)+28px)] px-10 pb-10 max-w-[1200px] mx-auto">
           {children}
         </div>
       </div>
-    </AdminGuard>
   );
 }
